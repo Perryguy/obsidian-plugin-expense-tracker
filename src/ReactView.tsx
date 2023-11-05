03bf2674f235f1609a6a5c8b79c9f603a110211e
Perryguy <srichardperry@gmail.com>
2023-11-05 16:32:07 +0000

    Component showing up in Obsidian

---
+++ src/ReactView.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const MyComponent = () => {
+  // Your JSX goes here
+  return <div>My Component</div>;
+};
+
+export default MyComponent;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+        // ... other rules
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+  };
+  
